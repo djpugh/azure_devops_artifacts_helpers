f7b179e3a22426270cd4d5eaa4b0a088ed3a3c26
David Pugh <djpugh@gmail.com>
2020-10-17 12:45:36 +0100

    Fixing default python version handling

--- populate_wheels.py
+++ populate_wheels.py
@@ -18,14 +18,14 @@ DOWNLOAD_INDEX_URL = os.environ.get('PIP_INDEX_URL', "https://pypi.org/simple")
 
 @click.command()
 @click.option('--index-url', default=DOWNLOAD_INDEX_URL, help='Index URL to use')
-@click.option('--py', 'python_versions', default=DEFAULT_PYTHON_VERSIONS, multiple=True, help='Python versions to populate')
+@click.option('--py', 'python_versions', default=DEFAULT_PYTHON_VERSIONS, multiple=True, help='Python versions to populate', type=str)
 @click.option('--target-dir', default=str(WHEELS_DIR), help='Target directory to output to')
 def populate_wheels(index_url=DOWNLOAD_INDEX_URL, python_versions=DEFAULT_PYTHON_VERSIONS, target_dir=str(WHEELS_DIR)):
     for py_version in python_versions:
         print('PLATFORM: {} - PYTHON VERSION: {}'.format(sys.platform, py_version))
         args = [sys.executable, '-m', 'pip', 'download',
                 '--only-binary=:all:',
-                '--python-version='+py_version,
+                '--python-version='+str(py_version),
                 '-d', target_dir,
                 '--index-url', index_url,
                 '-r', 'embed_requirements.txt']
