2e2e13bb2db93ae33a23b8a63b9f98e9ed65d598
David Pugh <djpugh@gmail.com>
2020-10-17 11:47:39 +0100

    Working on smell

--- tests/functional/virtualenv/test_virtualenv_init.py
+++ tests/functional/virtualenv/test_virtualenv_init.py
@@ -4,16 +4,18 @@ import subprocess
 import sys
 import unittest
 
+TEST_VENV_NAME = '.venv'
+
 if sys.platform.startswith('win'):
-    pip_path = Path('.venv')/'scripts'/'pip'
+    pip_path = Path(TEST_VENV_NAME)/'scripts'/'pip'
 else:
-    pip_path = Path('.venv')/'bin'/'pip'
+    pip_path = Path(TEST_VENV_NAME)/'bin'/'pip'
 
 class VirtualEnvTestCase(unittest.TestCase):
 
     def test_venv_pip(self):
         with tempfile.TemporaryDirectory() as td:
-            subprocess.check_call([sys.executable, '-m', 'virtualenv', f'{td}/.venv', '--seeder', 'azdo-pip'])
+            subprocess.check_call([sys.executable, '-m', 'virtualenv', str(Path(td)/TEST_VENV_NAME), '--seeder', 'azdo-pip'])
             installed_packages = subprocess.check_output([str(Path(td)/pip_path), 'freeze']).decode()
             self.assertIn('artifacts-keyring', installed_packages)
             self.assertIn('requests', installed_packages)
