88e9ce6422ef28e4f2bc60a151cdc051514bfb31
David J Pugh <6003255+djpugh@users.noreply.github.com>
2023-04-30 16:20:33 +0100

    Working on version resolution

--- src/azure_devops_artifacts_helpers/__init__.py
+++ src/azure_devops_artifacts_helpers/__init__.py
@@ -1,29 +1,4 @@
 """Package containing virtualenv helpers for installing artifacts-keyring."""
-__version__ = "0.0.0"  # Default blank Version
-try:
-    # Look here for setuptools scm to update the version - for development environments only
-    from setuptools_scm import get_version
-    try:
-        __version__ = get_version(root='../../', version_scheme='no-guess-dev', relative_to=__file__)
-    except LookupError:
-        pass
-except ImportError:
-    pass
-if __version__ == '0.0.0':
-    # Otherwise we try loading from file or metadata
-    try:
-        from azure_devops_artifacts_helpers._version import __version__  # noqa: F401
-    except ImportError:
-        pass
-if __version__ == '0.0.0':
-    # Use the metadata
-    import sys
-    if sys.version_info.major >= 3 and sys.version_info.minor >=8:
-        from importlib.metadata import version, PackageNotFoundError
-    else:
-        from importlib_metadata import version, PackageNotFoundError
-    try:
-        __version__ = version("azure_devops_artifacts_helpers")
-    except PackageNotFoundError:
-        # package is not installed
-        pass
+from azure_devops_artifacts_helpers._get_version import get_version
+__version__ = get_version()
+
---
+++ src/azure_devops_artifacts_helpers/_get_version.py
@@ -0,0 +1,35 @@
+def get_version():
+    """Get version information or return default if unable to do so."""
+    # Default
+    version = '0+unknown'
+    # Development installation only
+    try:
+        # Look here for setuptools scm to update the version - for development environments only
+        from setuptools_scm import get_version
+        try:
+            version = get_version(root='../../', version_scheme='no-guess-dev', relative_to=__file__)
+        except LookupError:
+            pass
+    except ImportError:
+        pass
+    # Development installation without setuptools_scm or installed package
+    # try loading from file
+    if version == '0+unknown':
+        try:
+            from azure_devops_artifacts_helpers._version import __version__  # noqa: F401
+        except ImportError:
+            pass
+    # Development installation without setuptools_scm
+    if version == '0+unknown':
+        # Use the metadata
+        import sys
+        if sys.version_info.major >= 3 and sys.version_info.minor >=8:
+            from importlib.metadata import version, PackageNotFoundError
+        else:
+            from importlib_metadata import version, PackageNotFoundError
+        try:
+            version = version("azure_devops_artifacts_helpers")
+        except PackageNotFoundError:
+            # package is not installed
+            pass
+    return version
