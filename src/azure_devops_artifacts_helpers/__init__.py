6ae17a7f17db7af6efc7ba1c4ca6eb633f3f5c57
David J Pugh <6003255+djpugh@users.noreply.github.com>
2023-04-30 15:24:34 +0100

    Removing version file from git tracking

--- src/azure_devops_artifacts_helpers/__init__.py
+++ src/azure_devops_artifacts_helpers/__init__.py
@@ -1,2 +1,14 @@
 """Package containing virtualenv helpers for installing artifacts-keyring."""
-from azure_devops_artifacts_helpers._version import __version__  # noqa: F401
+try:
+    from azure_devops_artifacts_helpers._version import __version__  # noqa: F401
+except ImportError:
+    import sys
+    if sys.version_info.major >= 3 and sys.version_info.minor >=8:
+        from importlib.metadata import version, PackageNotFoundError
+    else:
+        from importlib_metadata import version, PackageNotFoundError
+    try:
+        __version__ = version("azure_devops_artifacts_helpers")
+    except PackageNotFoundError:
+        # package is not installed
+        pass
