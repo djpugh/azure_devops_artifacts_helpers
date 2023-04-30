da8ff1786de5093ce09de88e44e16d82a7489554
David J Pugh <6003255+djpugh@users.noreply.github.com>
2023-04-30 15:58:39 +0100

    Working on version resolution

--- src/azure_devops_artifacts_helpers/__init__.py
+++ src/azure_devops_artifacts_helpers/__init__.py
@@ -1,7 +1,22 @@
 """Package containing virtualenv helpers for installing artifacts-keyring."""
+__version__ = "0.0.0"  # Default blank Version
 try:
-    from azure_devops_artifacts_helpers._version import __version__  # noqa: F401
+    # Look here for setuptools scm to update the version - for development environments only
+    from setuptools_scm import get_version, LookupError
+    try:
+        __version__ = get_version(root='../../../', version_scheme='no-guess-dev', relative_to='__file__')
+    except LookupError:
+        pass
 except ImportError:
+    pass
+if __version__ == '0.0.0':
+    # Otherwise we try loading from file or metadata
+    try:
+        from azure_devops_artifacts_helpers._version import __version__  # noqa: F401
+    except ImportError:
+        pass
+if __version__ == '0.0.0':
+    # Use the metadata
     import sys
     if sys.version_info.major >= 3 and sys.version_info.minor >=8:
         from importlib.metadata import version, PackageNotFoundError
