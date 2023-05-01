c03828a09b9c1657e4cad1cdba854511dc993ad0
David J Pugh <6003255+djpugh@users.noreply.github.com>
2023-05-01 23:57:12 +0100

    Streamlining wheel collation (#87)

--- tools/populate-wheels.py
+++ tools/populate-wheels.py
@@ -19,8 +19,6 @@ WHEELS_DIR = Path(__file__).parent.parent.absolute()/'src'/'azure_devops_artifac
 DOWNLOAD_INDEX_URL = os.environ.get('PIP_INDEX_URL', "https://pypi.org/simple")
 
 
-
-
 @click.command()
 @click.option('--index-url', default=DOWNLOAD_INDEX_URL, help='Index URL to use')
 @click.option('--py', 'python_versions', default=None, multiple=True, help='Python versions to populate, defaults to values set in pyproject.toml', type=str)
@@ -34,21 +32,20 @@ def populate_wheels(index_url: str = DOWNLOAD_INDEX_URL, python_versions: List[s
     dependencies = pyproject_toml['project']['requires-python']
     if not python_versions:
         # This works for same minor version only - assuming no major version change
-        min_version, max_version = required_python_versions.split(',')
+        _min_version, _max_version = required_python_versions.split(',')
         # Version
-        if min_version.startswith('>='):
-            min_version = Version(min_version.lstrip('>='))
-        elif min_version.startswith('>'):
-            _v = Version(min_version.lstrip('>'))
+        if _min_version.startswith('>='):
+            min_version = Version(_min_version.lstrip('>='))
+        elif _min_version.startswith('>'):
+            _v = Version(_min_version.lstrip('>'))
             min_version = Version(f'{_v.major}.{_v.minor+1}')
-        if max_version.startswith('<='):
-            max_version = Version(max_version.lstrip('<='))
-        elif max_version.startswith('<'):
-            _v = Version(max_version.lstrip('<'))
+        if _max_version.startswith('<='):
+            max_version = Version(_max_version.lstrip('<='))
+        elif _max_version.startswith('<'):
+            _v = Version(_max_version.lstrip('<'))
             max_version = Version(f'{_v.major}.{_v.minor-1}')
-        python_versions = [f'{min_version.major}.{minor}' for minor in range(min_version.minor, max_version.minor)]
-
-
+        python_versions = [f'{min_version.major}.{minor}' for minor in range(min_version.minor, max_version.minor+1)]
+        print(f'Using versions: {python_versions} from {_min_version}:{_max_version}')
     for py_version in python_versions:
         print(f'PLATFORM: {sys.platform} - PYTHON VERSION: {py_version}')
         args = [sys.executable, '-m', 'pip', 'download',
